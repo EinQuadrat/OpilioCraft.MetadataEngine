6e706e90a1da817d39ba8e9ec27846ce294d8d6b
Carsten Schäfer <carsten.schaefer@mailbox.org>
2025-03-21 20:17:16 +0100

    Prepare merge.

---
+++ Cmdlets/Repair-FileDate.fs
@@ -0,0 +1,32 @@
+﻿namespace OpilioCraft.MetadataEngine.Cmdlets
+
+open System
+open System.Management.Automation
+
+open OpilioCraft.FSharp.PowerShell
+open OpilioCraft.MetadataEngine.Core
+open OpilioCraft.MetadataEngine.RulesExtension
+
+// ------------------------------------------------------------------------------------------------
+
+[<Cmdlet(VerbsDiagnostic.Repair, "FileDate")>]
+[<OutputType(typeof<unit>)>]
+type public RepairFileDateCommand () =
+    inherit PathExpectingCommand ()
+
+    // cmdlet behaviour
+    override x.ProcessPath path =
+        // get metadata
+        let metadata =
+            MetadataEngine.extractMetadata path
+            |> PredefinedRules.applyMediaRules
+
+        // correct file timestamps
+        if metadata.Details.ContainsKey(Slot.DateTaken)
+            then
+                let dateTaken = metadata.Details.["DateTaken"].AsDateTime
+                IO.File.SetCreationTimeUtc(path, dateTaken)
+                IO.File.SetLastWriteTimeUtc(path, dateTaken)
+                IO.File.SetLastAccessTimeUtc(path, dateTaken)
+            else
+                x.WriteWarning($"cannot reset file date due to missing DateTaken: {path}")
