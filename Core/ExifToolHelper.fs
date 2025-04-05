a466899490f7a99061a82732051e2d9054d448cc
Carsten Schäfer <carsten.schaefer@mailbox.org>
2025-04-05 14:26:32 +0200

    Bugfix: Corrected approach to determine path of exiftool.

--- Core/ExifToolHelper.fs
+++ Core/ExifToolHelper.fs
@@ -37,7 +37,7 @@ type ExifToolCommand =
     | Quit
 
 module internal ExifTool =
-    let exifExecutable = Path.Combine(Settings.RuntimeBase, "exiftool.exe")
+    let exifExecutable = Path.Combine(Path.GetDirectoryName(Environment.ProcessPath), "exiftool.exe")
     let exifArgsFile = Path.Combine(Path.GetTempPath(), $"exiftool_{Environment.ProcessId}_args.txt")
 
     do
